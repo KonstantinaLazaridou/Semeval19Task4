c6270e68ce7ffe94bb216f1f58073fc1e297839a
konstantina.lazaridou <konstantina.lazaridou@hpi.de>
2018-11-08 16:24:56 +0100

    printing TP, FP, TN, FN

--- semeval/evaluation_metrics.py
+++ semeval/evaluation_metrics.py
@@ -13,3 +13,19 @@ def compute_confusion_matrix(result, pos_label):
     acc = accuracy_score(y_true=targets, y_pred=predictions);
     print("Precision={}, Recall={}, F1={}, Accuracy={}".format(pr, rec, f1, acc));
     print("{0:.2f}\t{1:.2f}\t{2:.2f}\t{3:.2f}\n".format(pr, rec, f1, acc));
+    tp = 0;
+    fp = 0;
+    fn = 0;
+    tn = 0;
+    for index, row in result.iterrows():
+        if row["predicted"] == row["actual"]:
+            if row["predicted"] == 1 and row["actual"] == 1:
+                tp += 1;
+            elif row["predicted"] == 0 and row["actual"] == 0:
+                tn += 1;
+        else:
+            if row["predicted"] == 1 and row["actual"] == 0:
+                fp += 1;
+            elif row["predicted"] == 0 and row["actual"] == 1:
+                fn += 1;
+    print("tp = {}, fp = {}, fn = {}, tn = {}".format(tp, fp, fn, tn));
